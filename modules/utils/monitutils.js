c024881e4f58b9ad6de3be74ec418a54a5884420
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-06-12 20:18:31 -0300

    Refatorando utils

--- modules/utils/monitutils.js
+++ modules/utils/monitutils.js
@@ -1,4 +1,3 @@
-'use strict'
 const shared_account_id = 16715013
 const adminIds = [
   16715013,
@@ -17,19 +16,19 @@ const notifySharedAccount = (bot, txt) => {
 }
 
 const notifyBlacklistedEval = (msg, bot, service) => {
-  const userinfo = msg.from.first_name + ' ' + msg.from.last_name + ' | @' + msg.from.username
-  const evalInfo = '`' + msg.text + '`'
-  notifyAdmins(bot, 'Eval malicioso recebido: ' + evalInfo + '\nEnviado por: ' + userinfo + '\nService: ' + service)
+  const userinfo = `${msg.from.first_name} ${msg.from.last_name} | @${msg.from.username}`
+  const evalInfo = `\`${msg.text}\``
+  notifyAdmins(bot, `Eval malicioso recebido: ${evalInfo}\nEnviado por: ${userinfo}\nService: ${service}`)
 }
 
 const isAdmin = (userId) => {
-  return adminIds.indexOf(userId) >= 0
+  return adminIds.includes(userId)
 }
 
-module.exports = {
+export default {
   adminIds,
   notifyAdmins,
   notifyBlacklistedEval,
   notifySharedAccount,
   isAdmin
-}
+};
